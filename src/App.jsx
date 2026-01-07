9ae033bc2e9e40bad0a0647224715a77b88f0ac1
Afnan Kaif <afnankaif31@gmail.com>
2026-01-07 23:19:56 +0530

    here

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agents from './pages/Agents'
+import Projects from './pages/projects'
+
+const App = () => {
+  return (
+
+    <div className='text-white'>
+      <Link to="/">Home</Link>
+      <Link to="/agents">Agents</Link>
+      <Link to="/projects">Projects</Link>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/agents' element={<Agents/>} />
+        <Route path='/' element={<Projects/> } />
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='h-screen w-screen bg-red-900'></div>
+    </div>
+  )
+}
+
+export default Home
